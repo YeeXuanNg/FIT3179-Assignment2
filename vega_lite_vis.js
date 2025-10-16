7065c177a59e86b184c06d20837e0e8cc8462ac9
YeeXuanNg <161012806+YeeXuanNg@users.noreply.github.com>
2025-10-16 15:08:54 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,24 @@
+var vg_1 = "overview_chart.vg.json";
+vegaEmbed("#overview_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as resut.view
+}).catch(console.error);
+
+var vg_2 = "water_consumption.vg.json";
+vegaEmbed("#water_consumption_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as resut.view
+}).catch(console.error);
+
+var vg_3 = "water_production.vg.json";
+vegaEmbed("#water_production_chart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as resut.view
+}).catch(console.error);
+
+var vg_4 = "water_resources_chart.vg.json";
+vegaEmbed("#water_resources_chart", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as resut.view
+}).catch(console.error);
+
+var vg_5 = "breakdown_chart.vg.json";
+vegaEmbed("#breakdown_chart", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as resut.view
+}).catch(console.error);
